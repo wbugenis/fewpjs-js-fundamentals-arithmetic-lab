2a9601e6be5045ced6fdbde6b760e1258c42447c
William Bugenis <wbugenis@gmail.com>
2021-02-24 10:58:58 -0500

    commit tree a7dffc8bf300b936dea86638f1335d35289e0a08

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-
-// Write your code here
+const num1 = 2
+const num2 = 31
+const multiply = num1 * num2
+const random = Math.round(Math.random() * 10)
+const num3 = 9
+const num4 = 5
+const mod = num3%num4
+const max = Math.max(1, 2, 20)
